75dbe0038c220cf7fe4e583ad28327c23c76c248
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-04-15 19:36:47 +0300

    calculations

---
+++ functions/calculator.py
@@ -0,0 +1,23 @@
+def add(x,y):
+    result =x+y
+    return result
+
+def subtract(x,y):
+    result =x-y
+    return result
+
+def divide(x,y):
+    result =x/y
+    return result
+
+def multiply(x,y):
+    result =x*y
+    return result
+
+def remainder(x,y):
+    result =x%y
+    return result
+
+def power_of(x,y):
+    result =x**y
+    return result
